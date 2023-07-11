e7c0dc96e8c2431e4a287c6529f412fadf5e9320
Facundoo15 <fangulos1@upao.edu.pe>
2023-07-10 22:26:58 -0500

    US08 MODIFICAR DATOS DEL USUARIO

---
+++ sit-api-vocacional/src/main/java/com/vocacional/sitapivocacional/repository/EstudianteRepository.java
@@ -0,0 +1,9 @@
+package com.vocacional.sitapivocacional.repository;
+
+import com.vocacional.sitapivocacional.model.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, String> {
+
+
+}
