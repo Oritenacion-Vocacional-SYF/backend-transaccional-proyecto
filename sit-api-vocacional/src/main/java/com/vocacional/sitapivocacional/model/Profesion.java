4870bad27ceba67c0160e6b46113425a861363c6
Jean Pierre Bocanegra <jeanbocanegra@intediperu.com>
2023-07-13 22:54:03 -0500

    La clase profesion creada

--- sit-api-vocacional/src/main/java/com/vocacional/sitapivocacional/model/Profesion.java
+++ sit-api-vocacional/src/main/java/com/vocacional/sitapivocacional/model/Profesion.java
@@ -1,3 +1,35 @@
 package com.vocacional.sitapivocacional.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Profesiones")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Profesion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 4)
+    private Long id_profesion;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String nombre;
+
+    @Column(length = 1000, nullable = false)
+    private String descripcion;
+
+    @Column(length = 100, nullable = false)
+    private String requisitos_admin;
+
+    @Column(length = 1000, nullable = false)
+    private String perspectiva_laboral;
+
+    @Column(length = 200, nullable = false)
+    private String salarios;
+
 }
