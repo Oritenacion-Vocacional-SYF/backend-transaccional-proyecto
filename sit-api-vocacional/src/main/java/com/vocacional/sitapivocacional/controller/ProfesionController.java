014163fdff8b1b9acf9b1c248a1017727093777d
Jean Pierre Bocanegra <jeanbocanegra@intediperu.com>
2023-07-13 23:33:20 -0500

    feat: Listar Profesiones

--- sit-api-vocacional/src/main/java/com/vocacional/sitapivocacional/controller/ProfesionController.java
+++ sit-api-vocacional/src/main/java/com/vocacional/sitapivocacional/controller/ProfesionController.java
@@ -24,4 +24,9 @@ public class ProfesionController {
         return (profesionService.addProfesion(profesion) != null) ? new ResponseEntity<>(profesion, HttpStatus.CREATED) : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Profesion>> listarProfesion() {
+        return ResponseEntity.ok(profesionService.getAllProfesion());
+    }
+
 }
